030b6dba582dc50385c8a5e7621b07dc196241a3
Marcin M <Marcin.Milewski.92@gmail.com>
2022-10-05 19:00:54 +0200

    Created GetAll and GetByName Methods in CelestialObject Controller

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -14,9 +17,8 @@ namespace StarChart.Controllers
             this._context = context;
         }
 
-        
-        [HttpGet("{id:int}")]
-        [Route("GetById")]
+
+        [HttpGet("{id:int}", Name = "Name")]
         public IActionResult GetById(int id)
         {
  
@@ -26,8 +28,76 @@ namespace StarChart.Controllers
                 return NotFound();
             }
 
+            foreach(var celestialObjectFromContext in _context.CelestialObjects)
+            {
+                if(celestialObjectFromContext.OrbitedObjectId == celestialObject.Id)
+                {
+                    if(celestialObject.Satellites == null)
+                    {
+                        celestialObject.Satellites = new List<CelestialObject>();
+                    }
+                    celestialObject.Satellites.Add(celestialObjectFromContext);
+                }
+            }
+
             return Ok(celestialObject);
         }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjects = _context.CelestialObjects.Where(c => c.Name == name);
+
+            if(celestialObjects == null || !celestialObjects.Any())
+            {
+                return NotFound();
+            }
+            foreach (var celestialObject in celestialObjects)
+            {
+                foreach (var celestialObjectFromContext in _context.CelestialObjects)
+                {
+                    if (celestialObjectFromContext.OrbitedObjectId == celestialObject.Id)
+                    {
+                        if (celestialObject.Satellites == null)
+                        {
+                            celestialObject.Satellites = new List<CelestialObject>();
+                        }
+                        celestialObject.Satellites.Add(celestialObjectFromContext);
+                    }
+                }
+            }
+
+                return Ok(celestialObjects);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects;
+
+            if (celestialObjects == null || !celestialObjects.Any())
+            {
+                return NotFound();
+            }
+            foreach (var celestialObject in celestialObjects)
+            {
+                foreach (var celestialObjectFromContext in _context.CelestialObjects)
+                {
+                    if (celestialObject.Satellites == null)
+                    {
+                        celestialObject.Satellites = new List<CelestialObject>();
+                    }
+                    if (celestialObjectFromContext.OrbitedObjectId == celestialObject.Id)
+                    {
+
+                        celestialObject.Satellites.Add(celestialObjectFromContext);
+                    }
+                }
+            }
+
+            return Ok(celestialObjects);
+        }
+
     }
 
 }
